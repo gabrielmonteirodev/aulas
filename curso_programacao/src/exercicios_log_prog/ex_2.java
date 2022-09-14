6c4228075a6a2cdda0a28904558d0436042ea9b3
Gabriel Monteiro <matos6542@gmail.comgit config --global user.name Gabriel>
2022-09-14 15:15:47 -0300

    atv2

---
+++ curso_programacao/src/exercicios_log_prog/ex_2.java
@@ -0,0 +1,91 @@
+package exercicios_log_prog;
+
+public class ex_2 {
+
+	public static void main(String[] args) {
+		
+		/*
+		Letra A)
+		int A, B;
+		
+		A = 10;
+		B = 20;
+		System.out.println("B = " + B);
+		B=5;
+		System.out.println("A = " + A + "," + " B = " + B);
+		*/
+		
+		/*
+		Letra B)
+		
+		
+		int A, B, C;
+		
+		A = 30;
+		B = 20;
+		C = A + B;
+		System.out.println("C = " + C);		
+		B=10;
+		System.out.println("B = " + B + "," + " C = " + C);
+		C = A + B;
+		System.out.println("A = " + A + "," + " B = " + B + "," + " C = " + C);
+		*/
+		
+		/* Letra C)
+		
+		int A , B, C;
+			
+		A = 10;
+		B = 20;
+		C = A;
+		B = C;
+		A = B;
+		
+		System.out.println("A = " + A + "," + " B = " + B + "," + " C = " + C);
+		*/
+		
+		/* Letra D)
+		int A , B;
+		
+		A = 10;
+		B = A +1;		
+		A = B +1;
+		B = A +1;	
+		
+		System.out.println("A = " + A);
+
+		A = B +1;
+		System.out.println("A = " + A + "," + " B = " + B);
+		*/
+		
+		/* Letra E)
+		
+		int A , B, C;
+		
+		A = 10;
+		B = 5;		
+		C = A +B;
+		B = 20;
+		A = 10;
+
+		System.out.println("A = " + A + "," + " B = " + B + "," + " C = " + C);
+		*/
+		
+		// Letra F)
+		
+		int X, Y, Z;
+		
+		X =1;
+		Y= 2;
+		Z = Y-X;
+		
+		System.out.println("Z = " + Z);
+		
+		X =5;
+		Y = X+Z;
+		
+		System.out.println("X = " + X + "," + " Y = " + Y + "," + " Z = " + Z);
+		
+	}
+
+}
