e9374c186eb7ab60e59b1e783bedbb98d8756a37
Gabriel Monteiro <matos6542@gmail.comgit config --global user.name Gabriel>
2022-09-14 15:12:12 -0300

    atvs

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int A, B, C ,D;
+		
+		A = 20;
+		B = 10;
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
+		C = B;
+		D = A;
+		System.out.println("A = " + C);
+		System.out.println("B = " + D);
+	}
+
+}
---
+++ curso_programacao/src/exercicios_log_prog/ex_1.java
@@ -0,0 +1,20 @@
+package exercicios_log_prog;
+
+public class ex_1 {
+
+		public static void main(String[] args) {
+			
+			int A, B, C ,D;
+			
+			A = 20;
+			B = 10;
+			System.out.println("A = " + A);
+			System.out.println("B = " + B);
+			C = B;
+			D = A;
+			System.out.println("A = " + C);
+			System.out.println("B = " + D);
+		}
+
+	}
+
